038ad588feb7019e2f45473d1ee599721b4cb236
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2025-05-17 08:27:21 +0530

    update

--- main.py
+++ main.py
@@ -10,7 +10,3 @@ async def read_root():
 async def health_check():
     return status.HTTP_200_OK
 
-# To run this (from your terminal, in the project root where main.py is):
-# uvicorn main:app --reload
-# Then open http://127.0.0.1:8000 in your browser
-# And http://127.0.0.1:8000/docs for automatic API documentation
